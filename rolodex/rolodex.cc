51fc80c59402865011d3456bb38bc916499bb697
alex <afein001@ucr.edu>
2014-01-13 18:18:07 -0800

    Started rolodex

---
+++ rolodex/rolodex.cc
@@ -0,0 +1,28 @@
+/*
+* Course: CS 100 Fall 2013
+*
+* First Name: Alex
+* Last Name: Feinland
+* Username: afein001
+* email address: afein001@ucr.edu
+*
+*
+* Assignment: HW1
+*
+* I hereby certify that the contents of this file represent
+* my own original individual work. Nowhere herein is there
+* code from any outside resources such as another individual,
+* a website, or publishings unless specifically designated as
+* permissible by the instructor or TA.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+}
+
